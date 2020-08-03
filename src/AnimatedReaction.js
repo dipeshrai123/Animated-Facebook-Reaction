13a4f8375e59b401d475508dc0573aa4706eeddb
raidipesh78 <raidipesh78@gmail.com>
2020-08-03 10:49:29 +0545

    Facebook reaction complete

--- src/AnimatedReaction.js
+++ src/AnimatedReaction.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { AnimatedBlock, useAnimatedValue, interpolate } from "react-uicomp";
 import {
   AiFillLike,
   AiFillHeart,
@@ -8,27 +9,127 @@ import {
 } from "react-icons/all";
 
 const AnimatedReaction = () => {
+  const [reactionVisible, setReactionVisible] = useState(false);
+  const reactionFive = useAnimatedValue(0, {
+    animationType: "elastic",
+  });
+  const reactionFour = useAnimatedValue(0, {
+    animationType: "elastic",
+    listener: function (value) {
+      reactionFive.value = value;
+    },
+  });
+  const reactionThree = useAnimatedValue(0, {
+    animationType: "elastic",
+    listener: function (value) {
+      reactionFour.value = value;
+    },
+  });
+  const reactionTwo = useAnimatedValue(0, {
+    animationType: "elastic",
+    listener: function (value) {
+      reactionThree.value = value;
+    },
+  });
+  const reactionOne = useAnimatedValue(0, {
+    animationType: "elastic",
+    listener: function (value) {
+      reactionTwo.value = value;
+    },
+  });
+  const containerOpacity = useAnimatedValue(reactionVisible, {
+    animationType: "elastic",
+    listener: function (value) {
+      reactionOne.value = value;
+    },
+  });
+
   return (
-    <div style={styles.container}>
-      <div style={styles.button}>
-        Hover Me
-        <div style={styles.reactionContainer}>
-          <div style={{ ...styles.reactionItem, backgroundColor: "#31A2FC" }}>
-            <AiFillLike />
-          </div>
-          <div style={{ ...styles.reactionItem, backgroundColor: "#F4405B" }}>
-            <AiFillHeart />
-          </div>
-          <div style={{ ...styles.reactionItem, backgroundColor: "#FCD55E" }}>
-            <FaLaughSquint />
-          </div>
-          <div style={{ ...styles.reactionItem, backgroundColor: "#FCD55E" }}>
-            <FaSadCry />
-          </div>
-          <div style={{ ...styles.reactionItem, backgroundColor: "#EB6012" }}>
-            <FaAngry />
-          </div>
+    <div
+      style={styles.container}
+      onMouseLeave={() => setReactionVisible(false)}
+    >
+      <div style={styles.reaction}>
+        <div
+          onMouseEnter={() => setReactionVisible(true)}
+          style={styles.button}
+        >
+          Hover Me
         </div>
+
+        <AnimatedBlock
+          style={{
+            ...styles.reactionContainer,
+            tranformOrigin: "0% 100%",
+            opacity: containerOpacity.value,
+          }}
+        >
+          <AnimatedBlock
+            style={{
+              ...styles.reactionItem,
+              backgroundColor: "#31A2FC",
+              opacity: reactionOne.value,
+              transform: interpolate(reactionOne.value, {
+                inputRange: [0, 1],
+                outputRange: ["translateY(20px)", "translateY(0px)"],
+              }),
+            }}
+          >
+            <AiFillLike />
+          </AnimatedBlock>
+          <AnimatedBlock
+            style={{
+              ...styles.reactionItem,
+              backgroundColor: "#F4405B",
+              opacity: reactionTwo.value,
+              transform: interpolate(reactionTwo.value, {
+                inputRange: [0, 1],
+                outputRange: ["translateY(20px)", "translateY(0px)"],
+              }),
+            }}
+          >
+            <AiFillHeart />
+          </AnimatedBlock>
+          <AnimatedBlock
+            style={{
+              ...styles.reactionItem,
+              backgroundColor: "#FCD55E",
+              opacity: reactionThree.value,
+              transform: interpolate(reactionThree.value, {
+                inputRange: [0, 1],
+                outputRange: ["translateY(20px)", "translateY(0px)"],
+              }),
+            }}
+          >
+            <FaLaughSquint />
+          </AnimatedBlock>
+          <AnimatedBlock
+            style={{
+              ...styles.reactionItem,
+              backgroundColor: "#FCD55E",
+              opacity: reactionFour.value,
+              transform: interpolate(reactionFour.value, {
+                inputRange: [0, 1],
+                outputRange: ["translateY(20px)", "translateY(0px)"],
+              }),
+            }}
+          >
+            <FaSadCry />
+          </AnimatedBlock>
+          <AnimatedBlock
+            style={{
+              ...styles.reactionItem,
+              backgroundColor: "#EB6012",
+              opacity: reactionFive.value,
+              transform: interpolate(reactionFive.value, {
+                inputRange: [0, 1],
+                outputRange: ["translateY(20px)", "translateY(0px)"],
+              }),
+            }}
+          >
+            <FaAngry />
+          </AnimatedBlock>
+        </AnimatedBlock>
       </div>
     </div>
   );
@@ -51,10 +152,15 @@ const styles = {
     textAlign: "center",
     fontFamily: "Arial",
     display: "block",
-    margin: "auto",
     cursor: "pointer",
     fontSize: 18,
+    margin: "auto",
     position: "relative",
+  },
+  reaction: {
+    width: 125,
+    position: "relative",
+    margin: "auto",
   },
   reactionContainer: {
     position: "absolute",
@@ -78,6 +184,7 @@ const styles = {
     alignItems: "center",
     color: "#ffffff",
     margin: "0px 10px",
+    cursor: "pointer",
     boxShadow: "0px 2px 4px rgba(0,0,0,0.12)",
   },
 };
